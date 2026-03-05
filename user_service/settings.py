a5a42cd124b1b1c5bacbab300c86547f592cbe23
Jason (Yue) Zhang <zhangyue1208@gmail.com>
2026-03-05 01:00:58 -0800

    refactor: auth and response cleanup 14

--- user_service/settings.py
+++ user_service/settings.py
@@ -142,7 +142,7 @@ SIMPLE_JWT = {
 # CORS Settings
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:3000,http://127.0.0.1:3000'
+    'http://localhost:3001,http://127.0.0.1:3001'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
